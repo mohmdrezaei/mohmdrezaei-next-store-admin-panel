3ba3faf20def32538bb0137cb7defb392fdd9fc4
mohmdrezaei <mhwmdrz9@gmail.com>
2024-11-04 21:18:02 +0330

    Bug fixes related to toastify

--- configs/api.js
+++ configs/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "../utils/cookie";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "http://localhost:3000/",
   headers: {
     "Content-Type": "application/json",
   },
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
 import TanstackQueryProvider from "../provider/TanstackQueryProvider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
 import '../styles/fonts.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <TanstackQueryProvider>
-
       <Component {...pageProps} />
+
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        rtl={true}
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </TanstackQueryProvider>
   );
 }
